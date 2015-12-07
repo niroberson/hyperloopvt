2121b53f4905d0735671f98ffe3df85df930afa0
NewBornSun <afl2592@vt.edu>
2015-12-06 23:20:23 -0500

    Updated digitalSignalGenerator.m: fixed variable names.

--- Navigation/digitalSignalGenerator.m
+++ Navigation/digitalSignalGenerator.m
@@ -8,43 +8,39 @@ s.Rate = rate;
 %% Define Output Waveform
 duration = 5;
 t = (1:(duration*rate))/rate;
+t_on = rate/600;
 
-% 1 second
-sampleRate = 300e3;
-t_on = sampleRate/600;
-
-outputN = zeros(1,(duration*sampleRate));
+outputN = zeros(1,(duration*rate));
 %t = 1:(duration*sampleRate);
 
 % 1 second
-for i = (sampleRate+1):(sampleRate+t_on+1)
+for i = (rate+1):(rate+t_on+1)
     outputN(i) = 4.5;
 end
 
 % Second Second
-for i = (2*sampleRate+2):(2*sampleRate + t_on+1)
+for i = (2*rate+2):(2*rate + t_on+1)
     outputN(i) = 4.5;
 end
 
 % Third Second
-for i = (3*sampleRate+2):(3*sampleRate + t_on+1)
+for i = (3*rate+2):(3*rate + t_on+1)
     outputN(i) = 4.5;
 end
-next = 3*sampleRate+(2*(t_on)+2)
+next = 3*rate+(2*(t_on)+2);
 for i = next:next+t_on+1
     outputN(i) = 4.5;
 end
 
-next1 = 3*sampleRate+(4*(t_on)+2)
+next1 = 3*rate+(4*(t_on)+2);
 for i = next1:next1+t_on+1
     outputN(i) = 4.5;
 end
 
-next2 = 3*sampleRate+(6*(t_on)+2)
+next2 = 3*rate+(6*(t_on)+2);
 for i = next2:next2+t_on+1
     outputN(i) = 4.5;
 end
-%output = 2*square(t,5)+2.1;
 %% Generate/Acquire continuous data
 queueOutputData(s,outputN');
 [data, timestamps, triggerTime] = s.startForeground;
