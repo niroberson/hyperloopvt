7c1885108fbcafc812aa3380dd80ea800d413da1
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-12 15:43:24 -0500

    Working Re function but needs updated to metric and dimensions

--- Aerodynamics/ReynoldsNumber.m
+++ Aerodynamics/ReynoldsNumber.m
@@ -1,28 +1,19 @@
+function Re = ReynoldsNumber()
 tube = load_spec('tube');
+pod = load_spec('pod');
+
 % Find the temperature inside the tube
-p_ambient = 101.325; % kPa
-T_ambient = (70 - 32) + 491.67; % Rankine
+p_tube = tube.p; % kPa
+T_tube = tube.T; % Rankine
 
-p = 0.1; %kPa
-T = p * T_ambient / p_ambient; % Temperature inside the tube
-
-% Calculate the viscocity 
-C = 120; % Sutherlands constant
-T0 = 524.07; % Rankine
-mu0 = 0.01827; % Centipoise
-a = 0.555*T0 + C;
-b = 0.555*T + C;
-mu = mu0 *(a/b)*(T/T0)^(3/2); % gram / (centimeter * second)
-mu = mu / 0.00220462; % gram to lb
-mu = mu * 0.0328084; % centimeter to ft
-
-% Calculate the denisty of the air inside the tube
+% Calculate the ambient density of air
 R = 10.73164; %(ft^3 * psia/ (Rankine * lb * mol)
-p = 0.0145038; % psia
-ro = p/(R*T); % lb * mol / ft^3
+mu = 1.983e-5;
+ro = p_tube/(R*T_tube); % kg*m&2/s^2
 
 % Calculate reynolds numbers
-v = 293.333; %ft/s % From 200 MPH
-d = 4.5; %ft
+v = 90; % m/s
+d = 4.5; % m
 Re = ro* v * d / mu; % Non-dimensional
+end
 
