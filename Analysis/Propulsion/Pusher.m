5f0f0ac72430eb39da32ce2cd3fa5bea06d36444
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-12 13:06:02 -0500

    Adding propulsion scripts

---
+++ Analysis/Propulsion/Pusher.m
@@ -0,0 +1,26 @@
+clear, clc
+
+% Acceleration provided by pusher
+
+M = [250, 500, 750, 1000, 1500, 2000, 2500, 3000, 4000, 5000]; %kg
+Ag = [2.4, 2, 1.7, 1.5, 1.2, 1, 0.9, 0.8, 0.6, 0.5]; %g
+A = Ag .* 9.81; %m/s^2
+D = 243.84; %m (800')
+T = sqrt(2 * D ./ A); %s
+V = A .* T; %m/s
+F = (M .* A)/1000; %kN
+
+m = 438.6; %kg
+a = interp1(M, A, m) %m/s^2
+v = interp1(M, V, m) %m/s
+f = interp1(M, F, m) %kN
+
+figure(1), hold on
+plot(M, F, 'm')
+[AX, H1, H2] = plotyy(M, A, M, V)
+xlabel('Mass (kg)')
+ylabel(AX(1), 'Max Velocity (m/s)')
+ylabel(AX(2), 'Acceleration (m/s^2), Force (N)')
+legend('Force', 'Acceleration', 'Velocity')
+plot([m, m], [0, 100], 'gr'); 
+title('Effect of Pod Mass on Pusher Characteristics')
