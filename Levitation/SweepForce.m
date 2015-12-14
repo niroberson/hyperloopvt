c65fa6365d9f0c8c6f9ce02ab54d45805c7536f7
antonmusk <antonya@vt.edu>
2015-12-14 17:04:43 -0500

    Update: in tandem with MagnetEquations.m

--- Levitation/SweepForce.m
+++ Levitation/SweepForce.m
@@ -7,35 +7,73 @@ mbound = 1e0; % bound for fourier sums
 nbound = 1e0;
 atol = 1e0; % desired tolerance
 rtol = 1e0;
-iternum = (2*mbound)+(2*nbound);
+
+% reference values from null flux paper for vx = 8.3:8.3:5*8.3
+liftREF = [0, 140,  262.5  337.5  380,  412];
+dragREF = [0, 26, 37.5, 44.5, 50, 55];
 
 tic % time start
 
 for vx = vlow:vres:vhigh
     if vx == vlow
-        [levF,dragF] = MagnetEquations(0,0,0,mbound,nbound,atol,rtol);
+        [levF,dragF,dragFx,dragFz] = magnetEQUATIONS(0,0,0,mbound,nbound,atol,rtol);
     else
-        [nextlev,nextdrag] = MagnetEquations(vx,0,0,mbound,nbound,atol,rtol);
+        [nextlev,nextdrag,Fx,Fz] = magnetEQUATIONS(vx,0,0,mbound,nbound,atol,rtol);
         levF = [levF,nextlev];
         dragF = [dragF,nextdrag];
+        dragFx = [dragFx,Fx];
+        dragFz = [dragFz,Fz];
     end
 end
 
-figure(1)
-subplot(1,2,1);
+figure
+subplot(2,2,1);
 scatter(vlow:vres:vhigh,levF,'filled');
 hold on
 plot(vlow:vres:vhigh,levF);
 ylabel('Lift Force [N]');
-xlabel('Velocity [m/s]');
+xlabel('Translational Velocity [m/s]');
 grid
 
-subplot(1,2,2);
+subplot(2,2,2);
 scatter(vlow:vres:vhigh,dragF,'filled');
 hold on
 plot(vlow:vres:vhigh,dragF);
 ylabel('Drag Force [N]');
-xlabel('Velocity [m/s]');
+xlabel('Translational Velocity [m/s]');
+grid
+
+subplot(2,2,3);
+plot(vlow:vres:vhigh,levF,'-o');
+hold on;
+plot(vlow:vres:vhigh,dragF,'-d');
+hold on;
+plot(vlow:vres:vhigh,abs(dragFx),'--d');
+hold on;
+plot(vlow:vres:vhigh,abs(dragFz),'--d');
+ylabel('Force [N]');
+xlabel('Translational Velocity [m/s]');
+axis([0 45 0 max(levF)*1.67])
+i=legend('lift','drag','trans drag ($$\hat{x}$$)','planar drag ($$\hat{z}$$)');
+set(i,'Interpreter','Latex');
+title('Lorentz Forces from Dual Halbach PM i-EC on Al Conductor');
+grid
+
+subplot(2,2,4);
+plot(vlow:vres:vhigh,liftREF,'--','LineWidth',2);
+hold on;
+plot(vlow:vres:vhigh,dragREF,'--','LineWidth',2);
+hold on;
+plot(vlow:vres:vhigh,levF,'-o');
+hold on;
+plot(vlow:vres:vhigh,dragF,'-d');
+hold on;
+ylabel('Force [N]');
+xlabel('Translational Velocity [m/s]');
+axis([0 45 0 max(liftREF)*1.67])
+i=legend('lift ref','drag ref','lift model','drag model');
+set(i,'Interpreter','Latex');
+title('Model Forces vs. True Values');
 grid
 
 toc % time stop
