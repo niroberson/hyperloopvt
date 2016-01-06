9f5736f66e48a4ffe381afe29d216c147fb626ed
Nathan Roberson <Nathan Roberson>
2016-01-05 20:58:04 -0500

    Adding commented out equations

--- Propulsion/ColdGasThruster.m
+++ Propulsion/ColdGasThruster.m
@@ -1,19 +1,43 @@
-function F = ColdGasThruster(mass_pod)
-%% Calculate the velocity and position of the pod during propulsion
-tube = load_spec('tube');
-%% Pressurized tank specs
-% TODO move this to specs file
-V_vessel = 6.45624; %volume of pressure vessel in m^3
-L = 1.27; %length of pressure vessel in m
+function Fth = ColdGasThruster(gas_spec)
+% Isp : Specific Impulse
+% Rsp (J/(kg*K)) : Specific Gas constant
+% Tc (K) : Chamber Temperature
+% m_dot (kg/s) : Mass flow
+% Ue (m/s) : Exhaust velocity
+% Pe (Pa) : Exit pressure
+% Pa (Pa) : Ambient pressure
+% Pc (Pa) : Chamber Pressure
+% Ae (m) : Exit Area (x-section)
+% gamma : Ratio of specific heats
 
 %% Load nitrogen spec
 nitrogen = load_spec('nitrogen');
 R = nitrogen.R; %specific gas constant for Nitrogen in J/kg*K
 gamma = nitrogen.gamma; %ratio of specific heats for Nitrogen (and air)
-rho_N = 20.4427; %density of Nitrogen in pressure vessel fully compressed in kg/m^3
+density = 20.4427; %density of Nitrogen in pressure vessel fully compressed in kg/m^3
 m_N = rho_N*V_vessel; %mass of Nitrogen in pressure vessel fully compressed in kg
 T_c = 300; %temperature of Nitrogen in tank in K
 c_star = 434.4*0.8; % m/s cstar at room temperature of nitrogen
+
+%% Rocket propulsion equations
+g = 9.81;
+
+% fth = m_dot*u_exhuast + (P_exit - P_ambient) * A_exit;
+% u_eq = u_exhuats + (P_exit - P_ambient)/m_dot*A_exit;
+% Fth = m_dot*u_eq;
+% Isp = u_eq/g;
+% Fth = m_dot*Isp*g;
+% lambda = (1+cos(alpha))/2 % alpha: half-angle of cone 15 degrees
+% Fth = lambda*m_dot*Isp*g;
+
+% Isp = 1 / g * sqrt( 2 * gamma / (gamma - 1) * Rsp * Tc * (1 - (P_exit / P_chamber)^((gamma - 1) / gamma)));
+%
+
+%% Thermodynamic equations
+% Ideal gas law with compressibility
+% P = Z*density*Rsp*T;
+
+
 
 %% Calulate the Thrust Force of the CGT
 P_c = 18202159.3; %gauge pressure in Pa
@@ -27,5 +51,5 @@ A_t = A_e/((1/M_e)*sqrt(((2/(gamma+1))*(1+((gamma-1)/2)*(M_e^2)))^((gamma+1)/(ga
 m_dot = P_c*A_t/c_star;
 A_c = V_vessel/L; %area of the converging nozzle in m^2
 % t_e = m_N/m_dot; %time gas is expelled in s
-F = m_dot*v_e + A_e*(P_e - P_ambient);
+Fth = m_dot*v_e + A_e*(P_e - P_ambient);
 end
