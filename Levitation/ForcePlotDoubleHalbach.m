11caa2fcf157a9a75cee1f48492ff95d14fabe3d
Alex <afl2592@gmail.com>
2016-01-07 18:13:53 -0500

    Added several parameters from different papers, comparing results between them

---
+++ Levitation/ForcePlotDoubleHalbach.m
@@ -0,0 +1,210 @@
+close all;
+clear all;
+% Drag parameter is dipping too quickly, for both values (Fig4,
+% 3D-Paper-Final)
+
+% Chose which figure to replicate
+parameters = '3D-Experimental'; % Fig4 or Fig7, 3D-Paper or Custom
+
+if(strcmp(parameters,'3D-Final'))
+    vfinal = 41;
+    tau_factor = 1.25;
+    geometry = 'Double';
+elseif(strcmp(parameters,'3D-Initial'))
+    vfinal = 41;
+    tau_factor = 0.045;
+    geometry = 'Double';
+elseif(strcmp(parameters,'3D-Experimental'))
+    vfinal = 41;
+    tau_factor = 0.15;
+    geometry = 'Double';
+elseif(strcmp(parameters,'2D-Paper'))
+    vfinal = 55;
+    tau_factor = 2;
+    geometry = 'Double';
+elseif(strcmp(parameters,'Second-2D'))
+    vfinal = 69;
+    tau_factor = 0.70;
+    geometry = 'Single';
+    
+% Characteristic analysis of electrodynamic suspension device
+% with permanent magnet Halbach array
+elseif(strcmp(parameters,'Third-2D'))
+    vfinal = 69;
+    tau_factor = 0.20;
+    geometry = 'Single';
+elseif(strcmp(parameters,'Fourth-2D'))
+    vfinal = 69;
+    tau_factor = 0.20;
+    geometry = 'Single';
+elseif(strcmp(parameters,'Magplane'))
+    vfinal = 60;
+    tau_factor = 0.35;
+    geometry = 'Single';
+end
+
+vres = 0.1;
+size = vfinal/0.1;
+
+F_lift = zeros(1,size);
+F_drag = zeros(1,size);
+n = zeros(1,size);
+i = 1;
+
+for v = 0.1:vres:vfinal
+    [Fy,Fz,lift_drag_ratio] = DoubleHalbachModel(v,parameters,geometry,tau_factor);
+    F_lift(1,i) = Fy;
+    F_drag(1,i) = (-1*Fz);
+    n(1,i) = lift_drag_ratio;
+    i = i + 1;
+end
+
+v = 0.1:vres:vfinal;
+v_kmh = v*3.6;
+
+if(strcmp(parameters,'3D-Final'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag/1000,v_kmh,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (kN)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 5])
+    set(ax(1),'YTick',[0:1:5])
+    set(ax(2),'YLim',[0 50])
+    set(ax(2),'YTick',[0:10:50])
+    set(ax(1),'XLim',[0 150])
+    set(ax(1),'XTick',[0:30:150])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+    
+elseif(strcmp(parameters,'3D-Initial'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag,v_kmh,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (N)') % label right y-axis
+    set(ax(1),'YLim',[0 90])
+    set(ax(1),'YTick',[0:30:90])
+    set(ax(2),'YLim',[0 450])
+    set(ax(2),'YTick',[0:150:450])
+    set(ax(1),'XLim',[0 150])
+    set(ax(1),'XTick',[0:30:150])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+    
+elseif(strcmp(parameters,'3D-Experimental'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag,v_kmh,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 1500])
+    set(ax(1),'YTick',[0:500:1500])
+    set(ax(2),'YLim',[0 1500])
+    set(ax(2),'YTick',[0:500:1500])
+    set(ax(1),'XLim',[0 25])
+    set(ax(1),'XTick',[0:5:25])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+    
+elseif(strcmp(parameters,'Fig4'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag,v_kmh,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 140])
+    set(ax(1),'YTick',[0:20:140])
+    set(ax(2),'YLim',[0 1.4])
+    set(ax(2),'YTick',[0:0.2:1.4])
+    set(ax(1),'XLim',[0 200])
+    set(ax(1),'XTick',[0:50:150])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+    
+elseif(strcmp(parameters,'Second-2D'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag/1000,v_kmh,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 2])
+    set(ax(1),'YTick',[0:0.5:2])
+    set(ax(2),'YLim',[0 6])
+    set(ax(2),'YTick',[0:1:6])
+    set(ax(1),'XLim',[0 250])
+    set(ax(1),'XTick',[0:50:250])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+elseif(strcmp(parameters,'Third-2D'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v_kmh,F_drag/1000,v_kmh,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (km/h)') % label x-axis
+    ylabel(ax(1),'Drag Force (kN)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 1])
+    set(ax(1),'YTick',[0:0.2:1])
+    set(ax(2),'YLim',[0 3])
+    set(ax(2),'YTick',[0:0.5:3])
+    set(ax(1),'XLim',[0 250])
+    set(ax(1),'XTick',[0:50:250])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v_kmh,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (km/h)');
+    grid on;
+    
+elseif(strcmp(parameters,'Magplane'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v,F_drag/1000,v,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)') % label x-axis
+    ylabel(ax(1),'Drag Force (kN)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 100])
+    set(ax(1),'YTick',[0:10:100])
+    set(ax(2),'YLim',[0 100])
+    set(ax(2),'YTick',[0:10:100])
+    set(ax(1),'XLim',[0 60])
+    set(ax(1),'XTick',[0:10:60])
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (m/s)');
+    grid on;
+end
