118d92d9e6466fdabdd9fe67954f5b0a94676e9e
Alex <afl2592@gmail.com>
2016-01-17 16:53:07 -0500

    Added stability parameters

--- Levitation/2D-AnalyticModel/DoubleHalbachModel.m
+++ Levitation/2D-AnalyticModel/DoubleHalbachModel.m
@@ -1,11 +1,17 @@
-function [Force_y, Force_z, n] = DoubleHalbachModel(v,parameters,geometry,tau_factor,M,tau,Br,h,l,rho_track,d1,d2,coeff)
+function [Force_y, Force_z, n] = DoubleHalbachModel(v,geometry,tau_factor,...
+                                 M,tau,Br,h,l,rho_track,d1,d2,coeff,...
+                                 profile,Jc,Jm)
 % Inputs:
 %   v: velocity (m/s)
 %   parameters: which values to use - '3D-Final', '3D'Initial', 'Fig4'
 %               'Fig7', 'Custom', '3D-Experimental'
 %   geometry: 'Single' vs. 'Double' sided arrays
 %   tau_factor: Adjust Force integral tau factor
-
+%   M: number of magnets in one array
+%   tau: pole pitch (m)
+%   Br: Magnet remanence (T)
+%   h: Magnet height (m)
+%   l: track thickness (m)
 
 %% Universal Constants 
 u0 = 4*pi*1e-7; % permeability of free space
@@ -28,14 +34,35 @@ B0 = Br*(1-exp(-p*h))*sin(pi/M)/(pi/M);
 Bsy = @(y,z) B0.*(exp(-p.*(y+d1)) - exp(-p.*(-y+d2+l))).*exp(1i*(p.*z + omega.*t));
 Bsz = @(y,z) B0.*(exp(-p.*(y+d1)) + exp(-p.*(-y+d2+l))).*exp(1i*(p.*z-pi/2+omega.*t));
 
+if(strcmp(profile,'Brakes'))
+    Bsy = @(y,z) B0.*(exp(-p.*(y+d1)) + exp(-p.*(-y+d2+l))).*exp(1i*(p.*z + omega.*t));
+    Bsz = @(y,z) B0.*(exp(-p.*(y+d1)) - exp(-p.*(-y+d2+l))).*exp(1i*(p.*z-pi/2+omega.*t));
+end
+
 if(strcmp(geometry,'Single'))
     Bsy = @(y,z) B0.*(exp(-p.*(y+d1))).*exp(1i*(p.*z + omega.*t));
     Bsz = @(y,z) B0.*(exp(-p.*(y+d1))).*exp(1i*(p.*z-pi/2+omega.*t));
+end 
+
+if(strcmp(profile,'Stability'))
+    Pavg = @(y) ((u0/4)*((Jc+Jm)/p)*((sinh(p*h))./cosh(p*(y+d1+h)))).^2;
+    Bsy = @(y,z) B0.*Pavg(y).*exp(1i*(p.*z + omega.*t));
+    Bsz = @(y,z) B0.*Pavg(y).*exp(1i*(p.*z-pi/2+omega.*t));
 end
 
 Bym = @(y) B0.*(exp(-p*(y+d1)) - exp(-p*(-y+d2+l)));
+
+if(strcmp(profile,'Brakes'))
+    Bym = @(y) B0.*(exp(-p*(y+d1)) + exp(-p*(-y+d2+1)));
+end
+
 if(strcmp(geometry,'Single'))
     Bym = @(y) B0.*(exp(-p*(y+d1)));
+    d2 = 0;
+end
+
+if(strcmp(profile,'Stability'))
+    Bym = @(y) B0.*(Pavg(y));
     d2 = 0;
 end
 
@@ -59,11 +86,32 @@ B = f*(-p*exp(-p*(d1+l)) + p*exp(-p*d2));
 C = f*(exp(-p*d1) - exp(-p*(d2+l)));
 D = f*(exp(-p*(d1+l)) - exp(-p*d2));
 
+if(strcmp(profile,'Brakes'))
+    A = f*(-p*exp(-p*d1) - p*exp(-p*(d2+l)));
+    B = f*(-p*exp(-p*(d1+l)) - p*exp(-p*d2));
+    C = f*(exp(-p*d1) + exp(-p*(d2+l)));
+    D = f*(exp(-p*(d1+l)) + exp(-p*d2));
+%     A = f*(-p*exp(-p*d1));% - p*exp(-p*(d2+l)));
+%     B = f*(-p*exp(-p*(d1+l)));% - p*exp(-p*d2));
+%     C = f*(exp(-p*d1));% + exp(-p*(d2+l)));
+%     D = f*(exp(-p*(d1+l)));% + exp(-p*d2));
+end
+
 if(strcmp(geometry,'Single'))
     A = f*(-p*exp(-p*d1));
     B = f*(-p*exp(-p*(d1+l)));
     C = f*(exp(-p*d1));
     D = f*(exp(-p*(d1+l)));
+end
+
+if(strcmp(profile,'Stability'))
+    c = (u0/4)*((Jc+Jm)/p);
+    d = sinh(p*h);
+    A = f*2*(c^2)*(d^2)*p*tanh(p*(d1+h))*sech(p*(d1+h))*sech(p*(d1+h));
+    B = f*2*(c^2)*(d^2)*p*tanh(p*(d1+h+l))*sech(p*(d1+h+l))*sech(p*(d1+h+l));
+    C = ((u0/4)*((Jc+Jm)/p)*((sinh(p*h))./cosh(p*(d1+h)))).^2;
+    D = ((u0/4)*((Jc+Jm)/p)*((sinh(p*h))./cosh(p*(l+d1+h)))).^2;
+    
 end
     
 % Coefficients
@@ -73,24 +121,20 @@ C6 = 0;
 % Coeff set new.
 % 
 % R1 = R2
+% C2 =(A + C*R2)/(R1 + R2);
+% 
+% C3 = (B*(R1 + R2) - A*b*(R2 + R3) + D*R1*R3 + D*R2*R3 + C*R1*R2*b...
+%       + C*R1*R3*b)/((R1 + R2)*(a*(R2 - R3) - b*(R2 + R3)));
+% 
+% C4 = (B*(R1 + R2) - a*(R2 - R3)*(A - C*R1) + D*R3*(R1 + R2))/((R1 + R2)...
+%       *(a*(R2 - R3) - b*(R2 + R3)));
+% 
+% C5 = (a*(R2*(D*(R1 + R2) - 2*A*b + 2*C*R1*b) + B*(R1 + R2))...
+%       + b*(R1 + R2)*(B - D*R2))/(c*(R1 + R2)*(a*(R2 - R3) - b*(R2 + R3)));
 
-% C2 = (C*R2^2*b*d - C*R2^2*a*d - 2*B*R2*d - 2*D*R2*R3*c - A*R3*a*c...
-%      + A*R2*a*d + A*R3*b*c + A*R2*b*d + C*R2*R3*a*c...
-%      + C*R2*R3*b*c)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c + R2*R3*a*c...
-%      + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
-%  
-% C3 = (B*R1*d - B*R2*d + D*R1*R3*c - D*R2*R3*c + A*R3*b*c + A*R2*b*d...
-%       - C*R1*R3*b*c - C*R1*R2*b*d)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c...
-%       + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
-%   
-% C4 = -(B*R1*d + B*R2*d + D*R1*R3*c + D*R2*R3*c + A*R3*a*c - A*R2*a*d...
-%      - C*R1*R3*a*c + C*R1*R2*a*d)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c...
-%      + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
-%  
-% C5 = (B*R1*a - B*R2*a - B*R1*b - B*R2*b - D*R2^2*a + D*R2^2*b...
-%       + D*R1*R2*a + D*R1*R2*b + 2*A*R2*a*b - 2*C*R1*R2*a*b)/(R2^2*b*d...
-%       - R2^2*a*d - R1*R3*a*c + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c...
-%       + R2*R3*b*c + R1*R2*b*d);
+% Latest/Best
+if(strcmp(coeff,'Set2'))
+    
 C2 = (-A*b*(R1 + R2) + a*(R1 - R2)*(A - C*R2)...
      - R2*(-2*B - 2*D*R1 + b*C*(R1 + R2)))/(a*(R1 - R2)^2 - b*(R1 + R2)^2);
 
@@ -104,24 +148,7 @@ C5 = (b*(R1 + R2)*(B - D*R2) + a*(B*(-R1 + R2)...
      + R2*(-2*A*b + 2*b*C*R1 - D*R1 + D*R2)))/(c*(a*(R1 - R2)^2 ...
      - b*(R1 + R2)^2));
 
-% Coefficients Set2
-% if(strcmp(coeff,'Set2'))
-% C2 = (A*R2*a - 2*D*R2*R3 - 2*B*R2 - A*R3*a + A*R2*b + A*R3*b - C*R2^2*a...
-%      + C*R2^2*b + C*R2*R3*a + C*R2*R3*b)/(R2^2*b - R2^2*a + R1*R2*a...
-%      - R1*R3*a + R2*R3*a + R1*R2*b + R1*R3*b + R2*R3*b);
-% 
-% C3 = (B*R1 - B*R2 + D*R1*R3 - D*R2*R3 + A*R2*b + A*R3*b - C*R1*R2*b...
-%       - C*R1*R3*b)/(R2^2*b - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a...
-%       + R1*R2*b + R1*R3*b + R2*R3*b);
-% 
-% C4 = -(B*R1 + B*R2 + D*R1*R3 + D*R2*R3 - A*R2*a + A*R3*a + C*R1*R2*a...
-%      - C*R1*R3*a)/(R2^2*b - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a...
-%      + R1*R2*b + R1*R3*b + R2*R3*b);
-%  
-% C5 = (B*R1*a - B*R2*a - B*R1*b - B*R2*b - D*R2^2*a + D*R2^2*b...
-%      + D*R1*R2*a + D*R1*R2*b + 2*A*R2*a*b - 2*C*R1*R2*a*b)/(c*(R2^2*b...
-%      - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a + R1*R2*b + R1*R3*b + R2*R3*b));
-% end
+end 
 
 theta = @(y) u0*gamma_two*v*Bym(y)./(k2_square);
 A_2x = @(y,z) (C3*exp(-R2.*y) + C4*exp(R2.*y) + theta(y)).*exp(1i*p.*z + omega.*t);
@@ -129,10 +156,23 @@ A_2x = @(y,z) (C3*exp(-R2.*y) + C4*exp(R2.*y) + theta(y)).*exp(1i*p.*z + omega.*
 By = @(y,z) Bsy(y,z) + 1i*p*A_2x(y,z);
 Bz = @(y,z) Bsz(y,z) - (-R2*C3*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
      +(f*(-p*exp(-p*(y+d1))+p*exp(-p*(-y+d2+l))))).*exp(1i.*p.*z + omega.*t);
- 
+
+if(strcmp(profile,'Brakes'))
+   Bz = @(y,z) Bsz(y,z) - (-R2*C3*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
+        +(f*(-p*exp(-p*(y+d1))-p*exp(-p*(-y+d2+l))))).*exp(1i.*p.*z + omega.*t); 
+%      Bz = @(y,z) Bsz(y,z) - (-R2*C3*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
+%           +(f*(-p*exp(-p*(y+d1))))).*exp(1i.*p.*z + omega.*t); 
+end
+
 if(strcmp(geometry,'Single'))
     Bz = @(y,z) Bsz(y,z) - (-R2*C3.*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
                 + (u0*gamma_two.*v.*B0.*(-p.*exp(-p.*(y+d1))))./(k2_square))...
+                .*exp(1i.*p.*z + omega.*t);
+end
+
+if(strcmp(profile,'Stability'))
+    Bz = @(y,z) Bsz(y,z) - (-R2*C3.*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
+                + (f.*2*(c^2)*(d^2)*p*tanh(p*(d1+h+y)).*sech(p*(d1+h+y)).*sech(p*(d1+h+y))))...
                 .*exp(1i.*p.*z + omega.*t);
 end
 
@@ -155,9 +195,9 @@ Force_z = .5*real(q);
 n = Force_y/(-1*Force_z);
 
 %% Scrap
-A_1x = @(y,z) (C1*exp(-R1.*y) + C2*exp(R1.*y)).*exp(1i*p.*z + omega.*t);
-A_3x = @(y,z) (C5*exp(-R3.*y) + C6*exp(R2.*y)).*exp(1i*p.*z + omega.*t);
-Bzm = @(y) B0.*(exp(-p*(y+d1)) + exp(-p*(-y+d2+l)));
+%A_1x = @(y,z) (C1*exp(-R1.*y) + C2*exp(R1.*y)).*exp(1i*p.*z + omega.*t);
+%A_3x = @(y,z) (C5*exp(-R3.*y) + C6*exp(R2.*y)).*exp(1i*p.*z + omega.*t);
+%Bzm = @(y) B0.*(exp(-p*(y+d1)) + exp(-p*(-y+d2+l)));
 
 % Coefficients Set1
 % Results dont look good with these. Keeping them here just incase...
@@ -180,4 +220,42 @@ Bzm = @(y) B0.*(exp(-p*(y+d1)) + exp(-p*(-y+d2+l)));
 % Bz2
 % Bz = @(y,z) Bsz(y,z) - (-R2*C3*exp(-R2.*y) + R2*C4.*exp(R2.*y) ...
 %      + (u0*gamma_two.*v.*B0.*(-p.*exp(-p.*(y+d1)) + p.*exp(-p.*(-y+d2+l))))./(k2_square))...
-%      .*exp(1i.*p.*z + omega.*t);
+%.*exp(1i.*p.*z + omega.*t);
+
+%%%%%%
+% C2 = (C*R2^2*b*d - C*R2^2*a*d - 2*B*R2*d - 2*D*R2*R3*c - A*R3*a*c...
+%      + A*R2*a*d + A*R3*b*c + A*R2*b*d + C*R2*R3*a*c...
+%      + C*R2*R3*b*c)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c + R2*R3*a*c...
+%      + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
+%  
+% C3 = (B*R1*d - B*R2*d + D*R1*R3*c - D*R2*R3*c + A*R3*b*c + A*R2*b*d...
+%       - C*R1*R3*b*c - C*R1*R2*b*d)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c...
+%       + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
+%   
+% C4 = -(B*R1*d + B*R2*d + D*R1*R3*c + D*R2*R3*c + A*R3*a*c - A*R2*a*d...
+%      - C*R1*R3*a*c + C*R1*R2*a*d)/(R2^2*b*d - R2^2*a*d - R1*R3*a*c...
+%      + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c + R2*R3*b*c + R1*R2*b*d);
+%  
+% C5 = (B*R1*a - B*R2*a - B*R1*b - B*R2*b - D*R2^2*a + D*R2^2*b...
+%       + D*R1*R2*a + D*R1*R2*b + 2*A*R2*a*b - 2*C*R1*R2*a*b)/(R2^2*b*d...
+%       - R2^2*a*d - R1*R3*a*c + R2*R3*a*c + R1*R2*a*d + R1*R3*b*c...
+%       + R2*R3*b*c + R1*R2*b*d);
+
+% Coefficients Set2
+% if(strcmp(coeff,'Set2'))
+% C2 = (A*R2*a - 2*D*R2*R3 - 2*B*R2 - A*R3*a + A*R2*b + A*R3*b - C*R2^2*a...
+%      + C*R2^2*b + C*R2*R3*a + C*R2*R3*b)/(R2^2*b - R2^2*a + R1*R2*a...
+%      - R1*R3*a + R2*R3*a + R1*R2*b + R1*R3*b + R2*R3*b);
+% 
+% C3 = (B*R1 - B*R2 + D*R1*R3 - D*R2*R3 + A*R2*b + A*R3*b - C*R1*R2*b...
+%       - C*R1*R3*b)/(R2^2*b - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a...
+%       + R1*R2*b + R1*R3*b + R2*R3*b);
+% 
+% C4 = -(B*R1 + B*R2 + D*R1*R3 + D*R2*R3 - A*R2*a + A*R3*a + C*R1*R2*a...
+%      - C*R1*R3*a)/(R2^2*b - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a...
+%      + R1*R2*b + R1*R3*b + R2*R3*b);
+%  
+% C5 = (B*R1*a - B*R2*a - B*R1*b - B*R2*b - D*R2^2*a + D*R2^2*b...
+%      + D*R1*R2*a + D*R1*R2*b + 2*A*R2*a*b - 2*C*R1*R2*a*b)/(c*(R2^2*b...
+%      - R2^2*a + R1*R2*a - R1*R3*a + R2*R3*a + R1*R2*b + R1*R3*b + R2*R3*b));
+% end
---
+++ Levitation/2D-AnalyticModel/HyperloopHalbachDesign.m
@@ -0,0 +1,410 @@
+%% Design for Hyperloop
+
+close all;
+clear all;
+
+%parameters = 'Hyperloop-Doubles';
+parameters = 'Hyperloop-Stilts';
+%parameters = 'Hyperloop-Brakes';
+%parameters = 'Hyperloop-Lateral';
+
+%profile = 'Brakes-';
+profile = 'Stability';
+coeff = 'Set2';
+    
+%% Test Bench Parameters
+
+if(strcmp(parameters,'Test-Rig'))
+    vfinal = 135;
+    tau_factor = 2;
+    numArrays = 1.25;
+    geometry = 'Double';
+    
+    % Magnet Parameters
+    M = 4; % Number of Magnets in Wavelength
+    tau = 0.2254; % Pole pitch (m)
+    Br = 1.48; % Magnet remanence (T)
+    h = 0.0127; % Heigh of permanent magnet (m)
+    width = .0127;
+    width = width*2;
+    
+    % Track Parameters
+    %l = 0.00047; % Thickness of track (m)
+    l = 0.003175; % 1/8th inch thick 
+    %rho_track = 3.92*1e-8; % Resistivity of track (Ohm*m)%
+    rho_track = 3.99*1e-8; % 6061 Resistivity
+
+    % Air gap Parameters
+    d1 = 0.015; % Upper air gap (m)
+    d2 = 0.030; % Lower air gap (m)
+end
+
+%% Hyperloop Design Specs   
+
+if(strcmp(parameters,'Hyperloop-Doubles'))
+    vfinal = 135;
+    tau_factor = 2;
+    width = .08;
+    %width = 0.0762;
+    numArrays = 6;
+    %numArrays = 1.25;
+    geometry = 'Double';
+    
+    % Magnet Parameters
+    M = 4; % Number of Magnets in Wavelength
+    lengthMag = 0.1016;
+    tau = 2*lengthMag;
+    tau = 0.15; % Pole pitch (m)
+   
+    Br = 1.32; % Magnet remanence (T)
+    h = 0.018; % Height of permanent magnet (m)
+    %h = 0.0254;
+    
+    % Track Parameters
+    l = 0.0104648; % Thickness of track (m)
+    rho_track = 3.99*1e-8; % Resistivity of track (Ohm*m)
+
+    % Air gap Parameters
+    d1 = 0.020; % Upper air gap (m)
+    d2 = 0.030; % Lower air gap (m)
+    
+    % Electromagnet Parameters
+    %Bre = ur*u0*Ne*Ie;
+    
+    % Pod Parameters
+    PodWeight = 2000; % Pod weight (N)
+
+elseif(strcmp(parameters,'Hyperloop-Stilts'))
+    vfinal = 135;
+    tau_factor = 2;
+    geometry = 'Stability';
+    
+    % Magnet Parameters
+    M = 4; % Number of Magnets in Wavelength
+    Hc = 11.2*1e3; % Coercive Force of Magnet
+    Br = 1; % Magnet remanence (T)
+   
+    tau = 0.0508; % Pole pitch (m)
+    h = 0.0254; % Height of permanent magnet (m)
+    width = .127; % Width of magnet (m)
+    numArrays = 1.25; % Number of arrays to simulate
+    lengthSingleMagnet = tau/2;
+    
+    % Track Parameters
+    l = 0.0127; % Thickness of track (m)
+    rho_track = 2.8*1e-8; % Resistivity of track (Ohm*m)
+
+    % Air gap Parameters
+    d1 = 0.020; % Upper air gap (m)
+    d2 = 0; % Lower air gap (m)
+    
+    % Electromagnet Parameters
+    %Bre = ur*u0*Ne*Ie;
+    
+    % Pod Parameters
+    PodWeight = 2000; % Pod weight (N)
+    Jc = 30*1e6; % Current density coil
+    Jm = h*Hc; % Current density magnet
+  
+elseif(strcmp(parameters,'Hyperloop-Brakes'))
+    vfinal = 135;
+    tau_factor = 2;
+    width = .08;
+    numArrays = 1.25;
+    geometry = 'Double';
+    
+    % Magnet Parameters
+    M = 4; % Number of Magnets in Wavelength
+    tau = 0.2; % Pole pitch (m)
+    Br = 1.48; % Magnet remanence (T)
+    h = 0.0225; % Heigh of permanent magnet (m)
+
+    % Track Parameters
+    l = 0.0079502; % Thickness of track (m)
+    rho_track = 3.99*1e-8; % Resistivity of track (Ohm*m)
+
+    % Air gap Parameters
+    d1 = 0.001; % Upper air gap (m)
+    d2 = 0.01; % Lower air gap (m)
+    
+elseif(strcmp(parameters,'Hyperloop-Lateral'))
+    vfinal = 135;
+    tau_factor = 2;
+    width = .04;
+    numArrays = 1;
+    geometry = 'Double';
+    
+    % Magnet Parameters
+    M = 4; % Number of Magnets in Wavelength
+    tau = 0.2; % Pole pitch (m)
+    Br = 1.48; % Magnet remanence (T)
+    h = 0.0225; % Heigh of permanent magnet (m)
+
+    % Track Parameters
+    l = 0.0079502; % Thickness of track (m)
+    rho_track = 3.99*1e-8; % Resistivity of track (Ohm*m)
+
+    % Air gap Parameters
+    d1 = 0.025; % Upper air gap (m)
+    d2 = 0.025; % Lower air gap (m)
+    
+end
+
+%% Weight and Cost Estimates
+
+volumeOneMagnet = h*width*(tau/2);
+volumeOneArray = volumeOneMagnet*M;
+volumeTotal = volumeOneArray*numArrays; % (m^2)
+
+densityNdFeB = 7500; % Neodymium density (kg/m^2)
+
+weightEstimate_kg = densityNdFeB*volumeTotal*2; % (kg)
+weightEstimate_lbs = densityNdFeB*volumeTotal*2.20462*2; % (lbs)
+if(strcmp(geometry,'Single'))
+    weightEstimate_kg = weightEstimate_kg/2;
+    weightEstimate_lbs = weightEstimate_lbs/2;
+end
+
+weightEstimate_kg
+weightEstimate_lbs
+
+NdFeB_PerPound = 325;
+costEstimate = weightEstimate_kg*NdFeB_PerPound % USD
+
+lbsToN = 4.448221628254617;
+MagnetWeight = 9.80665*weightEstimate_kg % (N)
+weight = MagnetWeight;
+
+%% Setup Simulation
+
+vres = 1;
+size = vfinal/vres;
+
+F_lift = zeros(1,size);
+F_drag = zeros(1,size);
+n = zeros(1,size);
+m = zeros(1,size);
+o = zeros(1,size);
+i = 1;
+
+for v = 0.1:vres:vfinal
+    [Fy,Fz,lift_drag_ratio] = DoubleHalbachModel(v,geometry,...
+                              tau_factor,M,tau,Br,h,l,rho_track,d1,d2,...
+                              coeff,profile,Jc,Jm);
+    F_lift(1,i) = Fy*width*numArrays;
+    F_drag(1,i) = (-1*Fz*width*numArrays);
+    n(1,i) = lift_drag_ratio;
+    m(1,i) = F_lift(1,i)/weight;
+    o(1,i) = (F_lift(1,i)-weight)/weight;
+    i = i + 1;
+end
+
+v = 0.1:vres:vfinal;
+v_kmh = v*3.6;
+v_mph = v*2.2;
+
+%% Hyperloop Related Plots
+if(strcmp(parameters,'Hyperloop-Doubles'))
+    Payload = F_lift(1,130) - PodWeight - MagnetWeight;
+    
+    subplot(2,2,1);
+    [ax,p1,p2] = plotyy(v,F_drag,...
+                        v,F_lift/1000,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)')  % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+    set(ax(1),'YLim',[0 700])
+    set(ax(1),'YTick',[0:100:700])
+    set(ax(2),'YLim',[0 7])
+    set(ax(2),'YTick',[0:1:7])
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:15:135])
+    set(ax(2),'XLim',[0 135])
+    set(ax(2),'XTick',[0:15:135])
+    grid on;
+    
+    if(strcmp(geometry,'Single'))
+        xlabel(ax(1),'Velocity (m/s)')  % label x-axis
+        ylabel(ax(1),'Drag Force (N)') % label left y-axis
+        ylabel(ax(2),'Lift Force (kN)') % label right y-axis
+        set(ax(1),'YLim',[0 20])
+        set(ax(1),'YTick',[0:5:20])
+        set(ax(2),'YLim',[0 20])
+        set(ax(2),'YTick',[0:5:20])
+        set(ax(1),'XLim',[0 135])
+        set(ax(1),'XTick',[0:15:135])
+        set(ax(2),'XLim',[0 135])
+        set(ax(2),'XTick',[0:15:135])
+        grid on;
+    end
+    
+    subplot(2,2,2);
+    [ax,p1,p2] = plotyy(v,((F_lift-weight)/1000),[0.1,vfinal],[Payload/1000,Payload/1000]);
+    title('Usable Lift Force');
+    xlabel(ax(1),'Velocity (m/s)')  % label x-axis
+    ylabel(ax(1),'Usable Lift Force (kN)') % label left y-axis
+    ylabel(ax(2),'Payload (kN)') % label right y-axis
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:25:135])
+    set(ax(1),'YLim',[0 7])
+    set(ax(1),'YTick',[0:.5:7])
+    set(ax(2),'YLim',[0 7])
+    set(ax(2),'YTick',[0:.5:7])
+    set(ax(2),'XLim',[0 135])
+    set(ax(2),'XTick',[0:15:135])
+    TeXString = texlabel('UsableForce = F_lift - MagnetWeight');
+    text(35,4,TeXString)
+    TeXString2 = texlabel('Payload = F_lift - PodWeight - MagnetWeight');
+    text(35,1,TeXString2)
+    grid on;
+    
+    subplot(2,2,3);
+    [ax,p1,p2]= plotyy(v,n,v,m);
+    title('Lift/Drag and Lift/Weight Ratios');
+    xlabel('Velocity (m/s)');
+    ylabel(ax(1),'Lift/Drag'); % label left y-axis
+    ylabel(ax(2),'Lift/Weight'); % label left y-axis
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:15:135])
+    set(ax(1),'YLim',[0 125])
+    set(ax(1),'YTick',[0:25:125])
+    set(ax(2),'YLim',[0 6])
+    set(ax(2),'YTick',[0:1:6])
+    set(ax(2),'XLim',[0 135])
+    set(ax(2),'XTick',[0:15:135])
+    grid on;
+    
+    subplot(2,2,4)
+    plot(v,o);
+    title('UsableLift/Weight Ratio');
+    xlabel('Velocity(m/s)');
+    grid on;
+    
+ elseif(strcmp(parameters,'Hyperloop-Stilts'))
+    Payload = F_lift(1,130) - PodWeight - MagnetWeight;
+    
+    subplot(3,1,1);
+    [ax,p1,p2] = plotyy(v,F_drag,...
+                        v,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)')  % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (N)') % label right y-axis
+    set(ax(1),'YLim',[0 100])
+    set(ax(1),'YTick',[0:10:100])
+    set(ax(2),'YLim',[0 100])
+    set(ax(2),'YTick',[0:10:100])
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:15:135])
+    set(ax(2),'XLim',[0 135])
+    set(ax(2),'XTick',[0:15:135])
+    grid on;
+    
+    subplot(3,1,2);
+    [ax,p1,p2]= plotyy(v,n,v,m);
+    title('Lift/Drag and Lift/Weight Ratios');
+    xlabel('Velocity (m/s)');
+    ylabel(ax(1),'Lift/Drag'); % label left y-axis
+    ylabel(ax(2),'Lift/Weight'); % label left y-axis
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:15:135])
+    set(ax(1),'YLim',[0 20])
+    set(ax(1),'YTick',[0:5:20])
+    set(ax(2),'YLim',[0 15])
+    set(ax(2),'YTick',[0:3:15])
+    set(ax(2),'XLim',[0 135])
+    set(ax(2),'XTick',[0:15:135])
+    grid on;
+    
+    subplot(3,1,3)
+    plot(v,o);
+    title('UsableLift/Weight Ratio');
+    xlabel('Velocity(m/s)');
+    grid on;
+    
+ elseif(strcmp(parameters,'Test-Rig'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v,F_drag,...
+                        v,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (N)') % label right y-axis
+    set(ax(1),'YLim',[0 100])
+    set(ax(1),'YTick',[0:10:100])
+    set(ax(2),'YLim',[0 100])
+    set(ax(2),'YTick',[0:10:100])
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:10:135])
+    TeXString = texlabel('d1 = 10mm, d2 = 15mm');
+    text(20,25,TeXString)
+    grid on;
+    
+    if(strcmp(geometry,'Single'))
+        set(ax(1),'YLim',[0 300])
+        set(ax(1),'YTick',[0:50:300])
+        set(ax(2),'YLim',[0 300])
+        set(ax(2),'YTick',[0:50:300])
+        set(ax(1),'XLim',[0 135])
+        set(ax(1),'XTick',[0:10:135]) 
+    end
+    
+    subplot(2,1,2);
+    plot(v,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (m/s)');
+    grid on;
+    
+elseif(strcmp(parameters,'Hyperloop-Brakes'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v,F_drag,...
+                        v,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (N)') % label right y-axis
+    set(ax(1),'YLim',[0 5000])
+    set(ax(1),'YTick',[0:1000:5000])
+    set(ax(2),'YLim',[0 5000])
+    set(ax(2),'YTick',[0:1000:5000])
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:10:135])
+    %TeXString = texlabel('d1 = 10mm, d2 = 15mm');
+    %text(20,25,TeXString)
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (m/s)');
+    grid on;
+    
+elseif(strcmp(parameters,'Hyperloop-Lateral'))
+    subplot(2,1,1);
+    [ax,p1,p2] = plotyy(v,F_drag,...
+                        v,F_lift,'plot','plot');
+    title('Lift and Drag Forces');
+    xlabel(ax(1),'Velocity (m/s)') % label x-axis
+    ylabel(ax(1),'Drag Force (N)') % label left y-axis
+    ylabel(ax(2),'Lift Force (N)') % label right y-axis
+    set(ax(1),'YLim',[0 500])
+    set(ax(1),'YTick',[0:100:500])
+    set(ax(2),'YLim',[0 500])
+    set(ax(2),'YTick',[0:100:500])
+    set(ax(1),'XLim',[0 135])
+    set(ax(1),'XTick',[0:10:135])
+    %TeXString = texlabel('d1 = 10mm, d2 = 15mm');
+    %text(20,25,TeXString)
+    grid on;
+    
+    subplot(2,1,2);
+    plot(v,n);
+    title('Lift/Drag Ratio');
+    xlabel('Velocity (m/s)');
+    grid on; 
+    
+end
+
+
+
