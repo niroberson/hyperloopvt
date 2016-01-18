19c7a810e3207b5051b6de68ed976257dd459b0d
Nathan Roberson <nathaniroberson@gmail.com>
2016-01-18 18:39:12 -0500

    Adding to testing

--- Propulsion/CGT.m
+++ Propulsion/CGT.m
@@ -20,12 +20,27 @@ mdott = rhot*At*Vt; % kg/s
 %% Exhaust Properties
 Pe = P0/(1+(k-1)/2*Me^2)^(k/(k-1)); % Pa
 Te = T0/(1+(k-1)/2*Me^2);
+
+% Be sure the gas is not liquifying
+if Te < 70
+    disp('Temperature at exhaust lower than 70K')
+end
+
 Z_e = 0.8637; % Needs to be a lookup or calculation
 rhoe = rho0*((Pe/P0)^(1/k));
 Ve = Vt*sqrt((k+1)/(k-1)*(1 - (Pe/P0)^((k-1)/k)));
 gam = sqrt(k)*(2/(k+1))^((k+1)/(2*(k-1)));
 Ae = At*gam/sqrt((2*k/(k-1))*(Pe/P0)^(2/k)*(1-(Pe/P0)^((k-1)/k)));
 mdote = rhoe*Ae*Ve;
+
+%% Assumptions
+if abs(mdote - mdott) >= 10^-2
+    disp('Error! Difference in mass flow between throat and exhaust')
+    mdote
+    mdott
+end
+
+%% Force thrust
 Fth = mdote*Ve;
 
 end
--- Propulsion/main.m
+++ Propulsion/main.m
@@ -1,4 +1,35 @@
+%% Actual configuration
+clear, clc
 P0 = 2.4132e+7; % Pa
 T0 = 300; % K
 At = (0.006 / 2)^2*pi; % m2
-Ac = (0.01905/2)^2*pi;
+Me = 2;
+Fth = CGT(P0, T0, Me, At);
+
+%% Sweep Area of Throat
+clear, clc
+P0 = 2.4132e+7; % Pa
+dt = 0.001:0.01:0.1;
+At = (dt / 2).^2*pi; % m2
+Me = 2;
+T0 = 300;
+F = [];
+for i=1:numel(At)
+    F(end+1) = CGT(P0, T0, Me, At(i));
+end
+
+figure, plot(At, F, 'r.')
+
+%% Testing Tank Temperature
+% CGT does not yet have isothermic or pressure drop off in tank model
+clear, clc
+P0 = 2.4132e+7; % Pa
+At = (0.006 / 2)^2*pi; % m2
+Me = 2;
+T0 = 250:10:300;
+F = [];
+for i=1:numel(T0)
+    F(end+1) = CGT(P0, T0(i), Me, At);
+end
+
+figure, plot(T0, F, 'r.')
