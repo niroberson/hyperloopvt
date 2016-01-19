7631db555c64e0f0b8e60f81a2b8a25437f2d692
Nathan Roberson <nathaniroberson@gmail.com>
2016-01-18 23:08:53 -0500

    Pressure vessel equations

--- Propulsion/Pressure_Vessel.m
+++ Propulsion/Pressure_Vessel.m
@@ -3,5 +3,16 @@
 % Assume (a) Isentropic and (b) Isothermal to determine
 % the decrease in tank pressure over time
 
+%% Gas constants
+k = 1.4;
+R = 297;
+
+
+%% Tank setup
+
+
 %% Isentropic
-t = -2*V*((pf/pi)^((1-k)/(2*k)) - 1)/((1-k)*R*sqrt(Ti)*A*sqrt(k/R*(2/(k+1))^((k+1)*(k-1))));
+t = @(pf, pi, Ti) -2*V*((pf/pi)^((1-k)/(2*k)) - 1)/((1-k)*R*sqrt(Ti)*A*sqrt(k/R*(2/(k+1))^((k+1)*(k-1))));
+
+%% Isothermal
+t =@(pf, pi) -V/(A*sqrt(k*R*T(2/(k+1))^((k+1)*(k-1))))*ln(pf/pi);
