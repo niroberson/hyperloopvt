3e2ca2c0b46b8611ce9b6898f6010d58628a0152
Nathan Roberson <nathaniroberson@gmail.com>
2016-01-27 05:31:08 -0500

    Surpressing plot output

---
+++ Propulsion/model/full_velocity_profile.m
@@ -0,0 +1,113 @@
+function full_velocity_profile()
+%% Constants
+l_track = 1609.34; % 5280 ft or 1 mile
+mPod = 500; % kg
+
+%% Set up global trackers
+gt = [];
+gv = [];
+gx = [];
+
+%% Experience pusher
+[xpush, tpush, Vpush] = pusher();
+gt = tpush;
+gx = xpush;
+gv = Vpush;
+
+%% Run propulsion
+vStart = Vpush(end);
+dt = 0.001;
+for t=0:dt:15
+    Fth = propulsion(t);
+    at = Fth/mPod;
+    vNext = vStart + 0.5*at*dt^2;
+    dx = (vNext^2 - vStart^2)/(2*at);
+    
+    % Track timestep
+    gt(end+1) = gt(end) + dt;
+    gx(end+1) = gx(end) + dx;
+    gv(end+1) = vNext;
+    
+    vStart = vNext;
+end
+
+%% Run Coast
+for t = 0:dt:15
+    
+end
+
+
+end
+
+%% Run Brakes
+function [xpush, tpush, Vpush] = pusher()
+%% Pusher Acceleration
+% v = v0 + 1/2at^2
+l_pusher= 243.84; % 800 ft
+xpush = 0:l_pusher;
+aPush = 2*9.8;
+Vpush = sqrt(2*aPush.*xpush);
+tpush = sqrt(Vpush(end)*2/aPush);
+end
+
+function Fth = propulsion(tprop)
+%% Nitrogen
+k = 1.4;
+R = 297;
+Fth = isentropic_time(k, R, tprop);
+end
+
+function Force_z = brake(v)
+    %% Calculate Eddy Brake Drag
+    profile = 'Brakes';
+    
+    width = .045; % Width (m)
+    tau = width/1.5; % Pole pitch (m)
+    h = 0.0635; % Heigh of permanent magnet (m)
+    P = 24;
+
+    % Track Parameters
+    l = 0.0079502; % Thickness of track (m)
+    rho_track = 3.99*1e-8; % Resistivity of track (Ohm*m)
+    
+    % Air gap Parameters
+    d1 = 0.010; % Upper air gap (m)
+    d2 = 0.010; % Lower air gap (m)
+    
+    [Force_y, Force_z, LtD, LtW, numMagnets, weightEstimate_kg,...
+     weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
+                            = DoubleHalbachModel(v,M,tau,Br,h,width,...
+                                                 P,l,rho_track,d1,d2,...
+                                                 coeff,profile,Jc,Jm);
+end
+
+function Force_z = magnetic_drag(v)
+%%
+width = 0.100; % Width of magnet (m)
+tau = width/1.5; % Pole pitch (m)
+h = tau*.4; % Height of permanent magnet (m)
+P = 5; % Number of arrays to simulate
+
+% Track Parameters
+l = 0.0127; % Thickness of track (m)
+rho_track = 2.8*1e-8; % Resistivity of track (Ohm*m)
+sigma = 1/rho_track;
+
+% Global Magnet Parameters
+M = 4; % Number of Magnets in Wavelength
+Br = 1.48; % Magnet remanence (T)
+Jc = 0;
+Jm = 0;    
+
+% Air gap Parameters
+d1 = 0.020; 
+d2 = 0;         % Lower air gap (m)    
+coeff = 'Set2';
+profile = 'Single';
+
+[Force_y, Force_z, LtD, LtW, numMagnets, weightEstimate_kg,...
+     weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
+                            = DoubleHalbachModel(v,M,tau,Br,h,width,...
+                                                 P,l,rho_track,d1,d2,...
+                                                 coeff,profile,Jc,Jm);
+end
--- Propulsion/model/isentropic_time.m
+++ Propulsion/model/isentropic_time.m
@@ -16,14 +16,14 @@ gamma = k*(2/(k+1))^((k+1)/(2*(k-1)));
 %% Choose Nozzle geometry based on initial conditions
 At = (4.5/1000)^2*pi;
 Me0 = 5;
-[Fth0, mdott0, Pe0, Ae] = CGT(Pi, Ti, Me0, At)
-re = sqrt(Ae/pi)
+[Fth0, mdott0, Pe0, Ae] = CGT(Pi, Ti, Me0, At);
+re = sqrt(Ae/pi);
 rt = sqrt(At/pi);
 re = sqrt(Ae/pi);
 alpha = 15*pi/180;
 beta = 60*pi/180;
-Ld = re/tan(alpha)
-Lc = rt/tan(beta)
+Ld = re/tan(alpha);
+Lc = rt/tan(beta);
 
 %% Time relationships
 P0 = @(t)Pi./(1+(k-1)/(2*k)*(k*R*Ti)^(1/2)*gamma*(At/V)*t).^(2*k/(k-1));
@@ -39,30 +39,30 @@ Fth = @(t) mdot(t).*Ve(t) + (Pe(t) - Pamb)*Ae;
 Mprop = @(t) V*Pi.^((k-1)/k)./(R*Ti)*P0(t).^(1/k);
 
 %% Mass Flow in System Decay
-figure
-[ax, p1, p2] = plotyy(t, Fth(t), t,  Mprop(t)/Mprop(0));
-xlabel(ax(1),'Time (s)')
-ylabel(ax(1),'Fth (N)')
-ylabel(ax(2), 'M(t)/Mi')
-title(' Thrust and Fuel Gauge')
+% figure
+% [ax, p1, p2] = plotyy(t, Fth(t), t,  Mprop(t)/Mprop(0));
+% xlabel(ax(1),'Time (s)')
+% ylabel(ax(1),'Fth (N)')
+% ylabel(ax(2), 'M(t)/Mi')
+% title(' Thrust and Fuel Gauge')
 
 %% Pressure and temperature at exit
-figure
-[ax,p1,p2] = plotyy(t, Pe(t),t,Te(t));
-xlabel(ax(1),'Time (s)')
-ylabel(ax(1),'Exit Pressure (Pa)')
-ylabel(ax(2),'Exit Temperature (K)')                                                                                                               
-title('Pressure and Temperature at Nozzle Exit')
-set(gca,'YMinorTick','on');
+% figure
+% [ax,p1,p2] = plotyy(t, Pe(t),t,Te(t));
+% xlabel(ax(1),'Time (s)')
+% ylabel(ax(1),'Exit Pressure (Pa)')
+% ylabel(ax(2),'Exit Temperature (K)')                                                                                                               
+% title('Pressure and Temperature at Nozzle Exit')
+% set(gca,'YMinorTick','on');
 
-%% Pressure and temperature at throat
-figure
-[ax,p1,p2] = plotyy(t, Pt(t),t,Tt(t));
-xlabel(ax(1),'Time (s)')
-ylabel(ax(1),'Throat Pressure (Pa)')
-ylabel(ax(2),'Throat Temperature (K)')                                                                                                               
-title('Pressure and Temperature at Nozzle Throat')
-set(gca,'YMinorTick','on');
+% %% Pressure and temperature at throat
+% figure
+% [ax,p1,p2] = plotyy(t, Pt(t),t,Tt(t));
+% xlabel(ax(1),'Time (s)')
+% ylabel(ax(1),'Throat Pressure (Pa)')
+% ylabel(ax(2),'Throat Temperature (K)')                                                                                                               
+% title('Pressure and Temperature at Nozzle Throat')
+% set(gca,'YMinorTick','on');
 
 %% Final Velocity
 % VPusher = 97.7677;
@@ -82,7 +82,6 @@ set(gca,'YMinorTick','on');
 % hold off
 
 %% Calculate total impulse
-I = sum(Fth(t))/numel(t)*t(end)
-Isp = Fth0/(mdott0*9.8)
-
+I = sum(Fth(t))/numel(t)*t(end);
+Isp = Fth0/(mdott0*9.8);
 Fthp = Fth(t);
