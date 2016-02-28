de196b45f3dd594aff44dff8efa987853e939fe8
Alex <afl2592@gmail.com>
2016-02-27 22:41:10 -0500

    Updated magnetic drag characteristics

--- Propulsion/full_velocity_profile.m
+++ Propulsion/full_velocity_profile.m
@@ -71,60 +71,25 @@ end
 
 function Force_z = brake(v)
     %% Calculate Eddy Brake Drag
-    profile = 'Brakes';
-    coeff = 'Set2';
-
-    width = .045; % Width (m)
-    tau = width/1.5; % Pole pitch (m)
-    h = 0.0635; % Heigh of permanent magnet (m)
-    P = 24;
-    % Global Magnet Parameters
-    M = 4; % Number of Magnets in Wavelength
-    Br = 1.48; % Magnet remanence (T)
-    Jc = 0;
-    Jm = 0;  
-    % Track Parameters
-    l = 0.0079502; % Thickness of track (m)
-    rho_track = 3.99*1e-8; % Resistivity of track (Ohm*m)
+    parameters = 'Hyperloop-Brakes';
     
-    % Air gap Parameters
-    d1 = 0.010; % Upper air gap (m)
-    d2 = 0.010; % Lower air gap (m)
+    [vfinal,profile,M,tau,Br,h,width,l,rho_track,d1,d2,P,PodWeight]...
+          = ParameterSelect(parameters);
     
     [Force_y, Force_z, LtD, LtW, numMagnets, weightEstimate_kg,...
-     weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
-                            = DoubleHalbachModel(v,M,tau,Br,h,width,...
-                                                 P,l,rho_track,d1,d2,...
-                                                 coeff,profile,Jc,Jm);
+          weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
+                                = DoubleHalbachModel(parameters,v,d1,h);
+    
 end
 
 function Force_z = magnetic_drag(v)
-%%
-width = 0.100; % Width of magnet (m)
-tau = width/1.5; % Pole pitch (m)
-h = tau*.4; % Height of permanent magnet (m)
-P = 5; % Number of arrays to simulate
-
-% Track Parameters
-l = 0.0127; % Thickness of track (m)
-rho_track = 2.8*1e-8; % Resistivity of track (Ohm*m)
-sigma = 1/rho_track;
-
-% Global Magnet Parameters
-M = 4; % Number of Magnets in Wavelength
-Br = 1.48; % Magnet remanence (T)
-Jc = 0;
-Jm = 0;    
-
-% Air gap Parameters
-d1 = 0.020; 
-d2 = 0;         % Lower air gap (m)    
-coeff = 'Set2';
-profile = 'Single';
-
-[Force_y, Force_z, LtD, LtW, numMagnets, weightEstimate_kg,...
-     weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
-                            = DoubleHalbachModel(v,M,tau,Br,h,width,...
-                                                 P,l,rho_track,d1,d2,...
-                                                 coeff,profile,Jc,Jm);
+    %% Calculate Levitation Drag
+    parameters = 'Hyperloop-Stilts';
+    
+    [vfinal,profile,M,tau,Br,h,width,l,rho_track,d1,d2,P,PodWeight]...
+          = ParameterSelect(parameters);
+    
+    [Force_y, Force_z, LtD, LtW, numMagnets, weightEstimate_kg,...
+          weightEstimate_lbs, length_feet, costEstimate, skinDepth]...
+                                = DoubleHalbachModel(parameters,v,d1,h);
 end
